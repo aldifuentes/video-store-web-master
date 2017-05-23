ac91ba73ac27b9d0490ef9b9b7e91ba6e04a22e0
aldifuentes <aldi_fuentes@hotmail.com>
2017-05-23 16:46:32 -0500

    Practico 8-AngularJSForm

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -37,3 +37,42 @@ app.controller('HomeController', ['$scope', function($scope) {
 
 }]);
 
+app.controller('ContactController', ContactController);
+app.controller('FeedbackController', FeedbackController);
+
+ContactController.$inject = ['$scope'];
+function ContactController ($scope) {
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
+}
+
+FeedbackController.$inject = ['$scope'];
+function FeedbackController ($scope) {
+    $scope.feedback = {
+        myChannel: "",
+        firstName: "",
+        lastName: "",
+        agree: false,
+        email: ""
+    }
+    $scope.sendFeedback = function() {
+        $scope.showMessage = false;
+        if ($scope.feedback.agree && ($scope.feedback.mychannel == "" ||
+            !$scope.feedback.mychannel)) {
+            $scope.invalidChannelSelection = true;
+        } else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback.mychannel= "";
+            $scope.feedback = {
+                mychannel: "",
+                firstName: "",
+                lastName: "",
+                agree: false,
+                email: ""
+            };
+            $scope.feedbackForm.$setPristine();
+            $scope.showMessage = true;
+        }
+    };
+}
